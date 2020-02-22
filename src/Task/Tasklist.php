27342783ec237ab2287f20fc85626c76cd061bcf
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-02-22 17:14:05 +0100

    Add default task list implementation

---
+++ src/Task/Tasklist.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpcq\Task;
+
+/**
+ * Default task list implementation.
+ */
+class Tasklist implements TasklistInterface
+{
+    /**
+     * @var TaskRunnerInterface[]
+     */
+    private $tasks;
+
+    public function add(TaskRunnerInterface $taskRunner): void
+    {
+        $this->tasks[] = $taskRunner;
+    }
+
+    public function getIterator(): iterable
+    {
+        yield from $this->tasks;
+    }
+}
