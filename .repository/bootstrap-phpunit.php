3023758d66b0d77b50ad9a54e489a260a25071ff
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-02-29 05:18:10 +0100

    Implement draft of config validation for phpunit

--- .repository/bootstrap-phpunit.php
+++ .repository/bootstrap-phpunit.php
@@ -9,8 +9,19 @@ return new class implements ConfigurationPluginInterface {
         return 'phpunit';
     }
 
+    private static $knownConfigKeys = [
+        'custom_flags' => 'custom_flags'
+    ];
+
     public function validateConfig(array $config) : void
     {
+        // Fix: we do not use the directories in this task, so we do not want to bail on them.
+        unset($config['directories']);
+        if ($diff = array_diff_key($config, self::$knownConfigKeys)) {
+            throw new \Phpcq\Exception\RuntimeException(
+                'Unknown config keys encountered: ' . implode(', ', array_keys($diff))
+            );
+        }
     }
 
     public function processConfig(array $config, BuildConfigInterface $buildConfig) : iterable
