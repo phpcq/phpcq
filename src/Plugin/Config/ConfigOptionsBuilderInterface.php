50a158b0c4f1dc46cdefaf6096f157db63aa1f31
David Molineus <david.molineus@netzmacht.de>
2020-03-25 14:30:08 +0100

    Adjust interface

--- src/Plugin/Config/ConfigOptionsBuilderInterface.php
+++ src/Plugin/Config/ConfigOptionsBuilderInterface.php
@@ -8,11 +8,11 @@ use Phpcq\Exception\InvalidConfigException;
 
 interface ConfigOptionsBuilderInterface
 {
-    public function describeArrayOption(string $name, string $description, array $defaultValue): self;
+    public function describeArrayOption(string $name, string $description, array $defaultValue = []): self;
 
-    public function describeIntOption(string $name, string $description, int $defaultValue): self;
+    public function describeIntOption(string $name, string $description, ?int $defaultValue = null): self;
 
-    public function describeStringOption(string $name, string $description, string $defaultValue): self;
+    public function describeStringOption(string $name, string $description, ?string $defaultValue = null): self;
 
     public function describeBoolOption(string $name, string $description, bool $defaultValue): self;
 
