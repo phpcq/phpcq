3be088c0be2baeb365799e23a3752eea010a78ab
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-04-04 08:01:46 +0200

    Load plugins via installed.json instead of Finder
    This also replaces `PluginRegistry::buildFromPath` with
    `PluginRegistry::buildFromInstalledJson`.
    This fixes the first step of #22

--- src/Plugin/PluginRegistry.php
+++ src/Plugin/PluginRegistry.php
@@ -2,10 +2,10 @@
 
 namespace Phpcq\Plugin;
 
+use Generator;
 use IteratorAggregate;
+use LogicException;
 use Phpcq\Exception\RuntimeException;
-use Symfony\Component\Finder\Finder;
-use function assert;
 use function get_class;
 
 final class PluginRegistry implements IteratorAggregate
@@ -13,17 +13,17 @@ final class PluginRegistry implements IteratorAggregate
     /** @var array<string, PluginInterface> */
     private $plugins = [];
 
-    public static function buildFromPath(string $pluginPath): self
+    public static function buildFromInstalledJson(string $installedJson): self
     {
         $instance = new self();
 
-        foreach (Finder::create()->in($pluginPath)->name('*.php')->getIterator() as $fileInfo) {
+        foreach (self::getBootstrapFileNames($installedJson) as $filePath) {
             /**
              * @psalm-suppress UnresolvableInclude
              *
              * @var PluginInterface
              */
-            $plugin = require $fileInfo->getRealPath();
+            $plugin = require_once $filePath;
             if (!$plugin instanceof PluginInterface) {
                 throw new RuntimeException('Not a valid plugin: ' . get_class($plugin));
             }
@@ -55,5 +55,33 @@ final class PluginRegistry implements IteratorAggregate
     public function getIterator(): iterable
     {
         yield from $this->plugins;
+    }
+
+    private static function getBootstrapFileNames(string $jsonFile): Generator
+    {
+        $realFile = realpath($jsonFile);
+        if (false === $realFile || !is_readable($realFile)) {
+            throw new RuntimeException('Invalid path provided: ' . $jsonFile);
+        }
+        $baseDir  = dirname($realFile);
+        $contents = json_decode(file_get_contents($jsonFile), true);
+        foreach ($contents['phars'] as $toolName => $toolVersions) {
+            if (count($toolVersions) > 1) {
+                throw new LogicException('Tool ' . $toolName . ' has multiple versions installed.');
+            }
+
+            $version   = $toolVersions[0];
+            $bootstrap = $version['bootstrap'];
+            if (!is_array($bootstrap) || 'file' !== $bootstrap['type']) {
+                throw new RuntimeException('Invalid bootstrap definition: ' . json_encode($bootstrap));
+            }
+            // Bootstrap files are in the same dir as the installed.json.
+            $path = realpath($baseDir . '/' . $bootstrap['url']);
+            if (false === $path || !is_readable($path)) {
+                throw new RuntimeException('Invalid bootstrap path provided for tool ' . $toolName . ': ' . $bootstrap['url']);
+            }
+
+            yield $path;
+        }
     }
 }
