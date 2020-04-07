56e2df50bbad810793fef133423ec6e7d4fa997a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-04-07 07:58:42 +0200

    Completely overhaul tool upgrading and downgrading

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -5,23 +5,25 @@ declare(strict_types=1);
 namespace Phpcq\Command;
 
 use Phpcq\ConfigLoader;
-use Phpcq\Exception\RuntimeException;
 use Phpcq\FileDownloader;
+use Phpcq\Output\SymfonyConsoleOutput;
+use Phpcq\Output\SymfonyOutput;
 use Phpcq\Platform\PlatformInformation;
-use Phpcq\Repository\JsonRepositoryDumper;
 use Phpcq\Repository\JsonRepositoryLoader;
-use Phpcq\Repository\Repository;
 use Phpcq\Repository\RepositoryFactory;
-use Phpcq\Repository\ToolHash;
-use Phpcq\Repository\ToolInformation;
+use Phpcq\ToolUpdate\UpdateCalculator;
+use Phpcq\ToolUpdate\UpdateExecutor;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use function assert;
 use function is_string;
 
 final class UpdateCommand extends AbstractCommand
 {
+    use InstalledRepositoryLoadingCommandTrait;
+
     protected function configure(): void
     {
         $this->setName('update')->setDescription('Update the phpcq installation');
@@ -32,6 +34,13 @@ final class UpdateCommand extends AbstractCommand
             'Path to the phpcq cache directory',
             (getenv('HOME') ?: sys_get_temp_dir()) . '/.cache/phpcq'
         );
+        $this->addOption(
+            'dry-run',
+            'd',
+            InputOption::VALUE_NONE,
+            'Dry run'
+        );
+
         parent::configure();
     }
 
@@ -59,50 +68,26 @@ final class UpdateCommand extends AbstractCommand
         $factory          = new RepositoryFactory($repositoryLoader);
         // Download repositories
         $pool = $factory->buildPool($config['repositories'] ?? []);
-        // Download needed tools and add to local repository.
-        $installed = new Repository($platformInformation);
-        foreach ($config['tools'] as $toolName => $tool) {
-            $toolInfo = $pool->getTool($toolName, $tool['version']);
-            // Download to destination path and add new information to installed repository.
-            $pharName = sprintf('%1$s~%2$s.phar', $toolInfo->getName(), $toolInfo->getVersion());
-            $downloader->downloadFileTo($toolInfo->getPharUrl(), $phpcqPath . '/' . $pharName);
 
-            $this->validateHash($phpcqPath . '/' . $pharName, $toolInfo->getHash());
-
-            $localTool = new ToolInformation(
-                $toolInfo->getName(),
-                $toolInfo->getVersion(),
-                $pharName,
-                $toolInfo->getPlatformRequirements(),
-                $toolInfo->getBootstrap(),
-                $toolInfo->getHash(),
-                $toolInfo->getSignatureUrl()
-            );
-
-            $installed->addVersion($localTool);
+        // Wrap console output
+        if ($output instanceof ConsoleOutputInterface) {
+            $consoleOutput = new SymfonyConsoleOutput($output);
+        } else {
+            $consoleOutput = new SymfonyOutput($output);
         }
-        // Save installed repository.
-        $dumper = new JsonRepositoryDumper($phpcqPath);
-        $dumper->dump($installed, 'installed.json');
+
+        $calculator = new UpdateCalculator($this->getInstalledRepository($phpcqPath), $pool, $consoleOutput);
+        $tasks = $calculator->calculate($config['tools']);
+
+        if ($input->hasOption('dry-run')) {
+            foreach ($tasks as $task) {
+                $output->writeln($task['message']);
+            }
+            return 0;
+        }
+        $executor = new UpdateExecutor($platformInformation, $downloader, $phpcqPath, $consoleOutput);
+        $executor->execute($tasks);
 
         return 0;
-    }
-
-    private function validateHash(string $pathToPhar, ?ToolHash $hash)
-    {
-        if (null === $hash) {
-            return;
-        }
-
-        static $hashMap = [
-            ToolHash::SHA_1   => 'sha1',
-            ToolHash::SHA_256 => 'sha256',
-            ToolHash::SHA_384 => 'sha384',
-            ToolHash::SHA_512 => 'sha512',
-        ];
-
-        if ($hash->getValue() !== hash_file($hashMap[$hash->getType()], $pathToPhar)) {
-            throw new RuntimeException('Invalid hash for file: ' . $pathToPhar);
-        }
     }
 }
