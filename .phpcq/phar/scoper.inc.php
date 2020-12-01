6c2b9f669fef1c69c4aa11425d63492da1e60629
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-12-01 09:29:06 +0100

    Whitelist symfony polyfills for global namespace

--- .phpcq/phar/scoper.inc.php
+++ .phpcq/phar/scoper.inc.php
@@ -2,8 +2,26 @@
 
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
+// Symfony polyfills must live in global namespace.
+$symfonyPolyfill = (static function (): array {
+    $files = [];
+    foreach (
+        Finder::create()
+            ->files()
+            ->in(__DIR__ . '/../../vendor/symfony/polyfill-*')
+            ->name('bootstrap.php') as $bootstrap
+    ) {
+        $files[] = $bootstrap->getPathName();
+    }
+
+    return $files;
+})();
+
 return [
     'whitelist' => [
         '\Phpcq\PluginApi\*'
     ],
+    'files-whitelist' => $symfonyPolyfill,
 ];
