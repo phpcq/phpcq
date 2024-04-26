d3927848a820dd8f1b75e292d5ab7fae490fcaea
David Molineus <david.molineus@netzmacht.de>
2024-04-26 16:05:48 +0200

    Add ConstraintUtil and corresponding tests
    This class provides a static matches method for comparing version string requirements against a set of constraints.

---
+++ src/Semver/ConstraintUtil.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpcq\Runner\Semver;
+
+use Composer\Semver\VersionParser;
+
+final class ConstraintUtil
+{
+    public static function matches(string $requirement, string $constraints): bool
+    {
+        $constraints = self::versionParser()->parseConstraints($constraints);
+        $requirement = self::versionParser()->parseConstraints($requirement);
+
+        return $constraints->matches($requirement);
+    }
+
+    private static function versionParser(): VersionParser
+    {
+        static $parser = null;
+
+        if ($parser === null) {
+            $parser = new VersionParser();
+        }
+
+        return $parser;
+    }
+}
